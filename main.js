c067daa32ea6ccb3388638703457d4830624faba
onhq11 <mateuszmatecki25@gmail.com>
2023-02-26 18:02:47 +0100

    Alpha v0.0.2
    - Added token validation
    - Added configuration in GUI

--- main.js
+++ main.js
@@ -1,29 +1,60 @@
 const { BrowserWindow, Menu, Tray, clipboard, app, screen, shell } = require('electron')
 const path = require('path')
 const Store = require('electron-store')
+const fs = require('fs')
 
 const schema = {
     firstConfig: {
-        type: 'boolean',
-        default: true
+        type: 'boolean'
     },
     clipboardSync: {
-        type: 'boolean',
-        default: false
+        type: 'boolean'
     },
     token: {
         type: 'string'
     },
     username: {
         type: 'string'
+    },
+    hostname: {
+        type: 'string'
     }
 }
 const store = new Store({schema})
 
+let notConfigured = false
+const fileExists = fs.existsSync('./config.json')
+if(fileExists) {
+    let config = fs.readFileSync('./config.json')
+    let parsed = JSON.parse(config)
+
+    if(parsed.token && parsed.hostname && parsed.username) {
+        store.set('token', parsed.token)
+        store.set('username', parsed.username)
+        store.set('hostname', parsed.hostname)
+        
+        store.set('firstConfig', false)
+        store.set('clipboardSync', true)
+    } else {
+        notConfigured = true
+    }
+} else {
+    notConfigured = true
+}
+
+if(notConfigured) {
+    store.set('token', '')
+    store.set('username', '')
+    store.set('hostname', '')
+    
+    store.set('firstConfig', true)
+    store.set('clipboardSync', false)
+}
+
 let copiedText = ''
 let copiedTextDate = ''
-let tray = null, win = null, configWin = null
-let trayVisible = false, syncBlocked = store.get('clipboardSync')
+let tray = null, win = null
+let trayVisible = false, syncBlocked = !store.get('clipboardSync')
 let dim = {
     width: null,
     height: null
@@ -54,7 +85,7 @@ app.on('ready', () => {
         { type: 'separator' },
         { label: 'Options', type: 'submenu', submenu: [
             { label: 'Clipboard Auto Sync', type: 'checkbox', checked: true, click(item) { if(item.checked) { syncBlocked = false } else { syncBlocked = true } } },
-            { label: 'Configuration', click() { settingsTab() } },
+            { label: 'Reconfigure', click() { reconfigure() } },
             { type: 'separator' },
             { label: 'Report bugs', click() { shell.openExternal("https://github.com/onhq11/TrayDrop/issues") } },
             { label: 'Docs', click() { shell.openExternal("https://github.com/onhq11/TrayDrop/blob/main/README.md") } }
@@ -85,7 +116,7 @@ app.on('ready', () => {
     })
 
     if(store.get('firstConfig')) {
-        configWin = new BrowserWindow({
+        let configWin = new BrowserWindow({
             width: 750,
             height: 600,
             show: true,
@@ -97,11 +128,20 @@ app.on('ready', () => {
             webPreferences: {
                 devTools: false,
                 preload: true,
-                zoomFactor: 0.8
+                zoomFactor: 0.8,
+                nodeIntegration: true,
+                enableRemoteModule: true,
+                contextIsolation:false
             }
         })
     
         configWin.loadFile('pages/config.html')
+
+        configWin.on('close', () => {
+            extractConfig()
+        })
+    } else {
+        tryLogin()
     }
 
     setInterval(syncClipboard, 5000)
@@ -125,6 +165,46 @@ const toggleTray = (show) => {
     }
 }
 
+const extractConfig = () => {
+    let getConfigWin = new BrowserWindow({
+        show: false
+    })
+
+    getConfigWin.loadFile('pages/config.html')
+
+    getConfigWin.webContents.executeJavaScript('localStorage.getItem("hostname")+"{SEPARATOR}"+localStorage.getItem("username")+"{SEPARATOR}"+localStorage.getItem("token")', true)
+    .then(result => {
+        let config = result.split('{SEPARATOR}')
+        let out = {
+            hostname: config[0],
+            username: config[1],
+            token: config[2]
+        }
+
+        fs.writeFileSync('./config.json', JSON.stringify(out))
+
+        syncBlocked = false
+        app.relaunch()
+        app.exit()
+    })
+}
+
+const tryLogin = () => {
+    fetch(store.get('hostname')+'/validateToken', {
+        method: 'POST',
+        body: JSON.stringify({"username": store.get('username')}),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': store.get('token')
+        }
+    }).then(response => response.json())
+    .then(response => {
+        if(response.error) {
+            reconfigure()
+        }
+    })
+}
+
 const syncClipboard = (forced) => {
     if(syncBlocked) {
         return
@@ -137,7 +217,7 @@ const syncClipboard = (forced) => {
         copiedTextDate = date
     }
 
-    fetch('http://localhost:30420/clipboard', {
+    fetch(store.get('hostname')+'/clipboard', {
         method: 'POST',
         body: JSON.stringify({"username": store.get('username'), "cbText": copiedText, "cbDate": copiedTextDate}),
         headers: {
@@ -179,6 +259,39 @@ const fadeWindowOut = (browserWindowToFadeOut) => {
     return interval
 }
 
-const settingsTab = () => {
+const reconfigure = () => {
+    fs.unlinkSync('./config.json')
+
+    let configWin = new BrowserWindow({
+        width: 750,
+        height: 600,
+        show: true,
+        frame: false,
+        resizable: true,
+        fullscreenable: false,
+        title: 'TrayDrop',
+        icon: path.join(__dirname, 'img/icon.png'),
+        webPreferences: {
+            devTools: false,
+            preload: true,
+            zoomFactor: 0.8,
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            contextIsolation:false
+        }
+    })
+
+    configWin.loadFile('pages/config.html')
+
+    configWin.on('close', () => {
+        extractConfig()
+    })
+
+    store.set('token', '')
+    store.set('username', '')
+    store.set('hostname', '')
+    
     store.set('firstConfig', true)
+    store.set('clipboardSync', false)
+    syncBlocked = true
 }
--- pages/script.js
+++ pages/script.js
@@ -20,15 +20,193 @@ document.getElementsByClassName('formBox')[0].addEventListener('submit', () => {
             }
 
             fetch(ip+'/ping', {
-                method: "POST",
-                mode: 'no-cors'
+                method: "POST"
             }).then(response => response.json())
             .then(response => {
-                console.log(response)
+                if(response.result === 'pong') {
+                    localStorage.clear()
+                    configPhase = 2
+                    showNotification('Server found', false)
+                    localStorage.setItem('hostname', ip)
+                    loadingEnd(true)
+                } else {
+                    showNotification('Server not found', true)
+                    loadingEnd()
+                }
+            })
+            .catch(err => {
+                showNotification('Server not found', true)
+                loadingEnd()
+            })
+            break
+
+        case 2:
+            document.getElementsByClassName('loadingAnim')[0].style.zIndex = 1
+            document.getElementsByClassName('loadingAnim')[0].style.opacity = 1.0
+            
+            document.getElementsByClassName('submitBtn')[0].style.opacity = 0.0
+
+            let login = {
+                username: document.getElementsByClassName('formText')[0].value,
+                password: document.getElementsByClassName('formText')[1].value
+            }
+
+            fetch(localStorage.getItem('hostname')+'/login', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(login)
+            }).then(response => response.json())
+            .then(response => {
+                if(response.error) {
+                    showNotification(response.error, true)
+                    loadingEnd()
+                    return
+                }
+
+                localStorage.setItem('username', document.getElementsByClassName('formText')[0].value)
+                localStorage.setItem('token', response.result)
+                configPhase = 3
+                loadingEnd(true)
+            })
+            .catch(err => {
+                showNotification('Server not found, try again', true)
+                loadingEnd()
+                setTimeout(function(){ location.reload() }, 3200)
+            })
+            break
+
+        case 2.5:
+            if(document.getElementsByClassName('formText')[1].value !== document.getElementsByClassName('formText')[2].value) {
+                showNotification('Passwords do not match', true)
+                return
+            }
+
+            document.getElementsByClassName('loadingAnim')[0].style.zIndex = 1
+            document.getElementsByClassName('loadingAnim')[0].style.opacity = 1.0
+            
+            document.getElementsByClassName('submitBtn')[0].style.opacity = 0.0
+
+            let register = {
+                username: document.getElementsByClassName('formText')[0].value,
+                password: document.getElementsByClassName('formText')[1].value
+            }
+
+            fetch(localStorage.getItem('hostname')+'/register', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(register)
+            }).then(response => response.json())
+            .then(response => {
+                if(response.error) {
+                    showNotification(response.error, true)
+                    loadingEnd()
+                    return
+                }
+
+                localStorage.setItem('token', response.result)
+                configPhase = 3
+                loadingEnd(true)
+            })
+            .catch(err => {
+                showNotification('Server not found, try again', true)
+                loadingEnd()
+                setTimeout(function(){ location.reload() }, 3200)
             })
             break
     }
 })
+
+function loadingEnd(passed) {
+    document.getElementsByClassName('submitBtn')[0].style.opacity = 1.0
+
+    document.getElementsByClassName('loadingAnim')[0].style.zIndex = -1
+    document.getElementsByClassName('loadingAnim')[0].style.opacity = 0.0
+
+    if(passed) {
+        document.getElementsByClassName('formBox')[0].style.transition = '0.2s'
+        document.getElementsByClassName('formBox')[0].style.opacity = 0.0
+
+        setTimeout(function(){
+            switch(configPhase) {
+                case 2:
+                    document.getElementsByClassName('formBox')[0].innerHTML = `
+                        <div class="formTextField">
+                            <input type="text" class="formText" required>
+                            <span></span>
+                            <label>Username</label>
+                        </div>
+                        <div class="formTextField">
+                            <input type="password" class="formText" required>
+                            <span></span>
+                            <label>Password</label>
+                        </div>
+                        <div class="loadingAnim"><div></div><div></div></div>
+                        
+                        <input type="submit" value="Log in" class="submitBtn">
+                        <div class="formHint">Don't have an account? <div onclick="configPhase = 2.5; loadingEnd(true)">Register</div></div>
+                        <div class="formHint"><div onclick="localStorage.setItem('clipboardSkipped', 'true'); configPhase = 3; loadingEnd(true)">Skip for now</div>` 
+
+                    document.getElementsByClassName('formText')[0].focus()                   
+                    break
+
+                case 2.5:
+                    document.getElementsByClassName('formBox')[0].innerHTML = `
+                        <div class="formTextField">
+                            <input type="text" class="formText" required>
+                            <span></span>
+                            <label>Username</label>
+                        </div>
+                        <div class="formTextField">
+                            <input type="password" class="formText" required>
+                            <span></span>
+                            <label>Password</label>
+                        </div>
+                        <div class="formTextField">
+                            <input type="password" class="formText" required>
+                            <span></span>
+                            <label>Confirm Password</label>
+                        </div>
+                        <div class="loadingAnim"><div></div><div></div></div>
+                        
+                        <input type="submit" value="Register" class="submitBtn">
+                        <div class="formHint">Already have an account? <div onclick="configPhase = 2; loadingEnd(true)">Log in</div></div>
+                        <div class="formHint"><div onclick="localStorage.setItem('clipboardSkipped', 'true'); configPhase = 3; loadingEnd(true)">Skip for now</div>`
+
+                    document.getElementsByClassName('formText')[0].focus()
+                    break
+
+                case 3:
+                    document.getElementsByClassName('formBox')[0].innerHTML = `
+                        <h2>Everything is ready</h2><br>
+                        <input type="button" value="Back to the app" class="submitBtn" id="wakeApp">`
+
+                    document.getElementById('wakeApp').addEventListener('click', () => {
+                        if(localStorage.getItem('token') && localStorage.getItem('hostname') || localStorage.getItem('clipboardSkipped')) {
+                            window.close()
+                        } else {
+                            showNotification('Cannot save auth data, try again later', true)
+                        }
+                    })
+                    break
+            }
+
+            document.getElementsByClassName('formBox')[0].style.transition = '0s'
+            document.getElementsByClassName('formBox')[0].style.marginLeft = '30vw'
+            document.getElementsByClassName('formBox')[0].style.marginRight = '-30vw'
+
+            setTimeout(function(){
+                document.getElementsByClassName('formBox')[0].style.transition = '0.5s'
+                document.getElementsByClassName('formBox')[0].style.opacity = 1.0
+                document.getElementsByClassName('formBox')[0].style.marginLeft = '0vw'
+                document.getElementsByClassName('formBox')[0].style.marginRight = '0vw'
+            }, 100)
+        }, 300)
+    }
+}
 
 function showNotification(message, error) {
     let animationDelay = 0
@@ -45,6 +223,12 @@ function showNotification(message, error) {
     setTimeout(function(){
         document.getElementsByClassName('popupTimeBar')[0].style.transition = '0s'
         document.getElementsByClassName('popupTimeBar')[0].style.width = '100%'
+
+        if(error) {
+            document.getElementsByClassName('popupIcon')[0].getElementsByTagName('img')[0].setAttribute('src', 'img/popupX.svg')
+        } else {
+            document.getElementsByClassName('popupIcon')[0].getElementsByTagName('img')[0].setAttribute('src', 'img/popupCheck.svg')
+        }
 
         document.getElementsByClassName('popupText')[0].innerHTML = message
     
@@ -63,29 +247,4 @@ function showNotification(message, error) {
             popupVisible = false
         }, 3000)
     }, animationDelay)
-}
-
-function getCookie(cname) {
-    let name = btoa(unescape(encodeURIComponent(cname))) + "="
-    let decodedCookie = decodeURIComponent(document.cookie)
-    let ca = decodedCookie.split(';')
-
-    for(let i = 0; i <ca.length; i++) {
-        let c = ca[i]
-
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1)
-        }
-
-        if (c.indexOf(name) == 0) {
-            return decodeURIComponent(c.substring(name.length, c.length))
-        }
-    }
-
-    return ''
-}
-
-function setCookie(cname, value) {
-    if(!value && value !== 0) { value = '' }
-    document.cookie = cname+'='+value
 }
